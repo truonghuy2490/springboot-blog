e1ce602156b25d82d4d57efe24d5e77d9a52bdfe
truonghuy2490 <huydocon1@gmail.com>
2023-10-14 21:52:47 +0700

    Security Dependency - Get All Post without @RequestParam by set default

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/CommentController.java
@@ -3,13 +3,14 @@ package com.springboot.blog.controller;
 import com.springboot.blog.entity.Comment;
 import com.springboot.blog.payload.CommentDTO;
 import com.springboot.blog.service.CommentService;
-import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/posts")
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -31,7 +31,7 @@ public class PostController {
     }
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) int pageNo,
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false)int pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
